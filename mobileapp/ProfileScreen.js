15f342cfd55f1dc3887167e2ddf4e9de167a7bb2
Miguel L. Arbesú <miguel.liar@gmail.com>
2021-03-10 22:36:54 +0100

    Location and views first implementation

--- mobileapp/ProfileScreen.js
+++ mobileapp/ProfileScreen.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, Button} from 'react-native';
 import { Card} from 'react-native-elements'
 import {Switch } from 'react-native-paper';
 import { DataTable, Avatar } from 'react-native-paper';
 import {HeaderBackButton} from '@react-navigation/stack';
+import * as Location from 'expo-location';
 import styles from './MyStyles'
 import MyMenu from './MyMenu'
 
@@ -20,6 +21,38 @@ export default function ProfileScreen({navigation}) {
       )
     });
   }, [navigation]);
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let {status} = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      if (!Location.hasStartedLocationUpdatesAsync()) {
+        setErrorMsg('Please, turn on your location');
+        return;
+      } else {
+        setErrorMsg(false);
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+
+  }
+  , []);
+
+  let text = 'Waiting...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location, null, '\t');
+  }
 
   return (
     <View style={styles.container}>
@@ -40,7 +73,23 @@ export default function ProfileScreen({navigation}) {
             <DataTable>
             <DataTable.Row>
                 <DataTable.Cell style={{flex: 3}}>Get location automatically:</DataTable.Cell>
-                <DataTable.Cell><MySwitch></MySwitch></DataTable.Cell>
+                <DataTable.Cell><MySwitch onToggleSwitch={() =>{
+                  if (this.MySwitch.isSwitchOn) {
+                    Location.stopLocationUpdatesAsync();
+                  } else {
+                    Location.startLocationUpdatesAsync();
+                  }
+                    }}></MySwitch></DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+                <DataTable.Cell>
+                <Button title='Get my position'onPress={() =>{
+                        Location.requestPermissionsAsync();
+                        alert(text); 
+                      }  
+                        }>Get My Position
+                </Button>
+                </DataTable.Cell>
             </DataTable.Row>
             </DataTable>
             
