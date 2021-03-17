d7c698b321ebbcef7ac566c116affa26f65bc887
UO270930 <marcostobias64@gmail.com>
2021-03-17 17:30:04 +0100

    public key stored in pod. (saves twice for some reason)

--- webapp/src/components/QRPage.js
+++ webapp/src/components/QRPage.js
@@ -3,15 +3,45 @@ import forge from 'node-forge';
 import QRCode from 'react-qr-code';
 import React, { useEffect, useState } from 'react';
 import { useSession } from "@inrupt/solid-ui-react";
+import {
+    getSolidDataset,
+    getThing,
+    setStringNoLocale,
+    addStringNoLocale,
+    setThing,
+    saveSolidDatasetAt,
+} from "@inrupt/solid-client";
+import { fetch } from "@inrupt/solid-client-authn-browser";
+import { FOAF, VCARD } from "@inrupt/vocab-common-rdf";
 
 export default function QRPage() {
     const { session } = useSession();
     const { webId } = session.info;
-    const [privateKey, setPrivateKey] = useState(()=>{
+
+
+    const [pair, setPair] = useState(() => {
         const pair = keypair({ bits: 1024 });
-        return pair.private;
+        console.log("done1");
+        return pair;
     });
-    return <QRCode
+
+
+
+
+
+    const privateKey = pair.private;
+    const publicKey = pair.public;
+
+    getSolidDataset(webId.split("#")[0], { fetch: fetch }).then((response) => {
+        const profile = getThing(response, webId);
+        let updatedProfile = setStringNoLocale(profile, VCARD.key, publicKey);
+        console.log(publicKey);
+        const myChangedDataset = setThing(response, updatedProfile);
+        const savedDataset = saveSolidDatasetAt(webId.split("#")[0], myChangedDataset, { fetch: fetch });
+    });
+
+
+    return <QRCode className="ml-5"
         level="Q"
         style={{ width: 256 }}
         value={JSON.stringify({
