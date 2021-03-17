541b4d661056c555b813ec831a229daccc3703f2
UO270930 <marcostobias64@gmail.com>
2021-03-17 21:52:48 +0100

    Fixes for codacy

--- mobileapp/LoginScreen.js
+++ mobileapp/LoginScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Image } from 'react-native';
 import { View, Text, TextInput, Button, Linking} from 'react-native';
-import { Card} from 'react-native-elements'
-import styles from './MyStyles'
+import { Card} from 'react-native-elements';
+import styles from './MyStyles';
 
 export default function LoginScreen({navigation}) {
   React.useLayoutEffect(() => {
@@ -25,10 +25,10 @@ export default function LoginScreen({navigation}) {
             <Card.Title style={styles.signinCardTitle}>Sign in</Card.Title>
             <Card.Divider/>
             <Text style={styles.normalText}>Introduce your provider URL:</Text>
-            <TextInput placeholder='https://inrupt.net' style={styles.textInput}></TextInput>
-            <Text style={styles.smallText}>Don't have one? You can get it here: <Text style={{color: '#3f51b5'}} onPress={() => {Linking.openURL('https://inrupt.com/')}}> Inrupt </Text> </Text>
+            <TextInput placeholder="https://inrupt.net" style={styles.textInput}></TextInput>
+            <Text style={styles.smallText}>Don't have one? You can get it here: <Text style={{color: '#3f51b5'}} onPress={() => {Linking.openURL("https://inrupt.com/");}}> Inrupt </Text> </Text>
             <View style={styles.cardButton}>
-            <Button color="#3f51b5" title='Log in'onPress={() => {navigation.navigate('Radarin')}}>Login</Button>
+            <Button color="#3f51b5" title="Log in" onPress={() => {navigation.navigate("Radarin");}}>Login</Button>
             </View>
       </Card>
       
