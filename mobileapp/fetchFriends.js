ab1b0be32180b1492afcaf30354f3f92f75590d2
UO270930 <marcostobias64@gmail.com>
2021-03-30 18:07:31 +0200

    More changes, fix in the QR page

--- mobileapp/fetchFriends.js
+++ mobileapp/fetchFriends.js
@@ -11,9 +11,9 @@ var fetcher;
 
 var parsedNames = [];
 
-export async function setWebId(navigation, webId){
-  webId = webId.replace('"','');
-  webId = webId.replace('"','');
+export async function setWebId(navigation, webId) {
+  webId = webId.replace('"', '');
+  webId = webId.replace('"', '');
   userId = webId;
   me = store.sym(userId);
   profile = me.doc();
@@ -26,34 +26,34 @@ export async function setWebId(navigation, webId){
     console.log("Loaded " + userName);
   }, err => {
     console.log("Load failed " + err);
-  }).then(async () => {await searchKnows()});
+  }).then(async () => { await searchKnows() });
 
   //Checks if the friendship is bidirectional for leaving out stalkers
-function isFriendship(name) {
-  let namesFriends = store.each(name, FOAF("knows"));
-  namesFriends.filter(fof => userId.includes(fof.value));
-  return namesFriends.length > 0;
-}
+  function isFriendship(name) {
+    let namesFriends = store.each(name, FOAF("knows"));
+    namesFriends.filter(fof => userId.includes(fof.value));
+    return namesFriends.length > 0;
+  }
 
-//Searchs for people that our user knows, adds them to the friend list only if they are indeed friends
-async function searchKnows(){
-  
-let names = store.each(me, FOAF("knows"));
+  //Searchs for people that our user knows, adds them to the friend list only if they are indeed friends
+  async function searchKnows() {
+
+    let names = store.each(me, FOAF("knows"));
     for (const name of names) {
       await fetcher.load(store.sym(name).doc()).then(() => {
-        if (isFriendship(name)){
+        if (isFriendship(name)) {
           var user = store.any(name, VCARD("fn"));
-          if (user == null){
+          if (user == null) {
             if (!parsedNames.includes(name.value))
               parsedNames.push(name.value);
-          }else{
+          } else {
             if (!parsedNames.includes(user.value))
               parsedNames.push(user.value);
           }
         }
       });
     }
-}
+  }
 }
 
 export function fetchFriends() {
