5e9e2cb53b422f486e02888526785d6ebeeaa162
user.entry <uo270927@uniovi.es>
2021-04-06 20:32:23 +0200

    Refactoring, changes made in profile and some experimenting done

--- mobileapp/ProfileScreen.js
+++ mobileapp/ProfileScreen.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import { View, Text, Button} from 'react-native';
 import { Card} from 'react-native-elements'
-import {Switch } from 'react-native-paper';
+import {Switch, RadioButton} from 'react-native-paper';
 import { DataTable, Avatar } from 'react-native-paper';
 import {HeaderBackButton} from '@react-navigation/stack';
-import * as Location from 'expo-location';
-import styles from './MyStyles'
-import MyMenu from './MyMenu'
+import styles from './MyStyles';
+import MyMenu from './MyMenu';
+import { getAsyncLocation } from './getAsyncLocations';
 import { sendLocation } from './sendLocation';
 
 export default function ProfileScreen({navigation}) {
@@ -22,39 +22,6 @@ export default function ProfileScreen({navigation}) {
       )
     });
   }, [navigation]);
-
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      let {status} = await Location.requestPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      if (!Location.hasStartedLocationUpdatesAsync()) {
-        setErrorMsg('Please, turn on your location');
-        return;
-      } else {
-        setErrorMsg(false);
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-
-  }
-  , []);
-
-  let text = 'Waiting for having a valid position...';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    
-    text = JSON.stringify(location, null, '\t');
-  }
 
   return (
     <View style={styles.container}>
@@ -75,26 +42,11 @@ export default function ProfileScreen({navigation}) {
             <DataTable>
             <DataTable.Row>
                 <DataTable.Cell style={{flex: 3}}>Get location automatically:</DataTable.Cell>
-                <DataTable.Cell><MySwitch onToggleSwitch={() =>{
-                  if (this.MySwitch.isSwitchOn) {
-                    Location.stopLocationUpdatesAsync();
-                    alert('This is on');
-                  } else {
-                    alert('This is off');
-                    Location.startLocationUpdatesAsync();
-                  }
-                    }}></MySwitch></DataTable.Cell>
+                <DataTable.Cell><MySwitch ></MySwitch></DataTable.Cell>
             </DataTable.Row>
             <DataTable.Row>
                 <DataTable.Cell>
-                <Button title='Get my position'onPress={() =>{
-                        Location.requestPermissionsAsync();
-                        if (location.coords !== null) {
-                          sendLocation(location.coords, location.timestamp);
-                        }
-                        alert(text); 
-                      }  
-                        }>Get My Position
+                <Button title='Get my position'onPress={() => getAsyncLocation.getLocation()}>Get My Position
                 </Button>
                 </DataTable.Cell>
             </DataTable.Row>
@@ -105,10 +57,18 @@ export default function ProfileScreen({navigation}) {
   );
 }
 
+
 const MySwitch = () => {
   const [isSwitchOn, setIsSwitchOn] = React.useState(false);
 
-  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+  const onToggleSwitch = () => {
+    setIsSwitchOn(!isSwitchOn);
+    if (isSwitchOn) {
+      sendLocation.getCurrentPositionAsync(isSwitchOn);
+    } else {
+      
+    }
+  }
 
   return <Switch color="#3f51b5" value={isSwitchOn} onValueChange={onToggleSwitch} />;
 };
--- mobileapp/getAsyncLocations.js
+++ mobileapp/getAsyncLocations.js
@@ -1,3 +1,6 @@
+import React, {useState, useEffect} from 'react';
+import * as Location from 'expo-location';
+
 const [location, setLocation] = useState(null);
 const [errorMsg, setErrorMsg] = useState(null);
 
@@ -27,10 +30,15 @@ let text = 'Waiting for having a valid position...';
 if (errorMsg) {
   text = errorMsg;
 } else if (location) {
-  
   text = JSON.stringify(location, null, '\t');
 }
 
 export async function getLocation() {
-
+  {
+    Location.requestPermissionsAsync();
+    if (location.coords !== null) {
+      sendLocation.sendLocation(location.coords, location.timestamp);
+    }
+    alert(text); 
+  }  
 }
