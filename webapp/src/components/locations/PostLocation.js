9463b9968309b032a90a85d3f9a244c33d41ba04
UO270930 <marcostobias64@gmail.com>
2021-04-23 20:01:48 +0200

    Maybe the photo works now

--- webapp/src/components/locations/PostLocation.js
+++ webapp/src/components/locations/PostLocation.js
@@ -1,59 +1,69 @@
 import getOrCreatePublicFilePod from "../../utils/GetOrCreatePublicFilePod";
-import { 
-    getSolidDataset, 
-    getThing, 
-    getUrlAll, 
-    getSourceUrl, 
-    addDatetime, 
-    addStringNoLocale, 
-    createThing, 
-    setThing, 
-    saveSolidDatasetAt 
+import {
+    getSolidDataset,
+    getThing,
+    getUrlAll,
+    getSourceUrl,
+    addDatetime,
+    addStringNoLocale,
+    createThing,
+    setThing,
+    saveSolidDatasetAt
 } from "@inrupt/solid-client";
 
 export default async function postLocation(session, title, description, photo) {
-    const profileDataset = await getSolidDataset(session.info.webId, {
-        fetch: session.fetch,
-    });
-    const profileThing = getThing(profileDataset, session.info.webId);
-    const podsUrls = getUrlAll(
-        profileThing,
-        "http://www.w3.org/ns/pim/space#storage"
-    );
-    const pod = podsUrls[0];
-    const containerUri = `${pod}public/RadarinLocations/`;
-    const dataset = await getOrCreatePublicFilePod(containerUri, session.fetch, "savedLocations.ttl");
+    const reader = new FileReader();
+    reader.readAsDataURL(photo);
 
-    const locationsUrl = getSourceUrl(dataset);
+    reader.onloadend = async function () {
+        const base64Photo = reader.result;
 
-    const locationWithDescription = addStringNoLocale(
-        createThing(),
-        "http://schema.org/text",
-        description
-    );
+        console.log(base64Photo);
 
 
-    const locationWithTitle = addStringNoLocale(
-        locationWithDescription,
-        "http://schema.org/text",
-        title
-    );
+        const profileDataset = await getSolidDataset(session.info.webId, {
+            fetch: session.fetch,
+        });
+        const profileThing = getThing(profileDataset, session.info.webId);
+        const podsUrls = getUrlAll(
+            profileThing,
+            "http://www.w3.org/ns/pim/space#storage"
+        );
+        const pod = podsUrls[0];
+        const containerUri = `${pod}public/RadarinLocations/`;
+        const dataset = await getOrCreatePublicFilePod(containerUri, session.fetch, "savedLocations.ttl");
 
-    const locationWithPhoto = addStringNoLocale(
-        locationWithTitle,
-        "http://xmlns.com/foaf/spec/#term_Image",
-        photo
-    );
+        const locationsUrl = getSourceUrl(dataset);
 
-    const locationWithTimestamp = addDatetime(
-        locationWithPhoto,
-        "http://www.w3.org/2002/12/cal/ical#created",
-        new Date()
-    );
+        const locationWithDescription = addStringNoLocale(
+            createThing(),
+            "http://schema.org/text",
+            description
+        );
 
-    const updatedLocationsList = setThing(dataset, locationWithTimestamp);
 
-    await saveSolidDatasetAt(locationsUrl, updatedLocationsList, { fetch: session.fetch });
+        const locationWithTitle = addStringNoLocale(
+            locationWithDescription,
+            "http://schema.org/text",
+            title
+        );
 
-    console.log("done");
+        const locationWithPhoto = addStringNoLocale(
+            locationWithTitle,
+            "http://schema.org/text",
+            base64Photo
+        );
+
+        const locationWithTimestamp = addDatetime(
+            locationWithPhoto,
+            "http://www.w3.org/2002/12/cal/ical#created",
+            new Date()
+        );
+
+        const updatedLocationsList = setThing(dataset, locationWithTimestamp);
+
+        await saveSolidDatasetAt(locationsUrl, updatedLocationsList, { fetch: session.fetch });
+
+        console.log("done");
+    }
 }
