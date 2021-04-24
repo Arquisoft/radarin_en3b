408dec663bd2f483b0eeef97aeacc63c5fea021b
UO270930 <marcostobias64@gmail.com>
2021-04-24 12:10:27 +0200

    Photos and URIs

--- webapp/src/components/locations/PostLocation.js
+++ webapp/src/components/locations/PostLocation.js
@@ -8,62 +8,66 @@ import {
     addStringNoLocale,
     createThing,
     setThing,
-    saveSolidDatasetAt
+    saveSolidDatasetAt,
+    overwriteFile
 } from "@inrupt/solid-client";
 
 export default async function postLocation(session, title, description, photo) {
-    const reader = new FileReader();
-    reader.readAsDataURL(photo);
 
-    reader.onloadend = async function () {
-        const base64Photo = reader.result;
+    const profileDataset = await getSolidDataset(session.info.webId, {
+        fetch: session.fetch,
+    });
+    const profileThing = getThing(profileDataset, session.info.webId);
+    const podsUrls = getUrlAll(
+        profileThing,
+        "http://www.w3.org/ns/pim/space#storage"
+    );
+    const pod = podsUrls[0];
+    const containerUri = `${pod}public/RadarinLocations_/`;
+    const dataset = await getOrCreatePublicFilePod(containerUri, session.fetch, "savedLocations.ttl");
 
-        console.log(base64Photo);
+    const photoName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
+    await overwriteFile(
+        `${containerUri}/${photoName}`,
+        photo,
+        { type: "image/png", fetch: session.fetch },
+    );
 
-        const profileDataset = await getSolidDataset(session.info.webId, {
-            fetch: session.fetch,
-        });
-        const profileThing = getThing(profileDataset, session.info.webId);
-        const podsUrls = getUrlAll(
-            profileThing,
-            "http://www.w3.org/ns/pim/space#storage"
-        );
-        const pod = podsUrls[0];
-        const containerUri = `${pod}public/RadarinLocations/`;
-        const dataset = await getOrCreatePublicFilePod(containerUri, session.fetch, "savedLocations.ttl");
-
-        const locationsUrl = getSourceUrl(dataset);
-
-        const locationWithDescription = addStringNoLocale(
-            createThing(),
-            "http://schema.org/text",
-            description
-        );
+    console.log("done");
 
 
-        const locationWithTitle = addStringNoLocale(
-            locationWithDescription,
-            "http://schema.org/text",
-            title
-        );
-
-        const locationWithPhoto = addStringNoLocale(
-            locationWithTitle,
-            "http://schema.org/text",
-            base64Photo
-        );
-
-        const locationWithTimestamp = addDatetime(
-            locationWithPhoto,
-            "http://www.w3.org/2002/12/cal/ical#created",
-            new Date()
-        );
-
-        const updatedLocationsList = setThing(dataset, locationWithTimestamp);
-
-        await saveSolidDatasetAt(locationsUrl, updatedLocationsList, { fetch: session.fetch });
-
-        console.log("done");
-    }
+    
+            const locationsUrl = getSourceUrl(dataset);
+    
+            const locationWithDescription = addStringNoLocale(
+                createThing(),
+                "http://schema.org/text",
+                description
+            );
+    
+    
+            const locationWithTitle = addStringNoLocale(
+                locationWithDescription,
+                "http://schema.org/text",
+                title
+            );
+    
+            const locationWithPhoto = addStringNoLocale(
+                locationWithTitle,
+                "http://schema.org/text",
+                photoName
+            );
+    
+            const locationWithTimestamp = addDatetime(
+                locationWithPhoto,
+                "http://www.w3.org/2002/12/cal/ical#created",
+                new Date()
+            );
+    
+            const updatedLocationsList = setThing(dataset, locationWithTimestamp);
+    
+            await saveSolidDatasetAt(locationsUrl, updatedLocationsList, { fetch: session.fetch });
+    
+            console.log("done");
 }
