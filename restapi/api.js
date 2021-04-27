737fb2a7ef8224c418a03878d38dacbc6fda8101
alvarorece <alvarorece@gmail.com>
2021-04-27 18:53:11 +0200

    API Rework and testing widely improved

--- restapi/api.js
+++ restapi/api.js
@@ -4,8 +4,6 @@ const TrackedLocation = require("./models/TrackedLocation");
 const router = express.Router();
 const webIdQueryChecker = require('./middleware/WebIdQueryChecker');
 
-router.get("/locations", webIdQueryChecker);
-
 router.post("/locations", async (req, res) => {
     const trackedLocation = new TrackedLocation({
         webId: req.claims.webid,
@@ -15,6 +13,8 @@ router.post("/locations", async (req, res) => {
     trackedLocation.save();
     res.send(trackedLocation);
 });
+
+router.get("/locations", webIdQueryChecker);
 
 router.get("/locations", async (req, res) => {
     const webId = req.claims.webid;
@@ -26,21 +26,6 @@ router.get("/locations", async (req, res) => {
 
     const userLocations = await TrackedLocation.find({ webId }).sort({ timestamp: -1 });
     res.send(userLocations);
-});
-
-router.get("/friendslocations", webIdQueryChecker);
-
-router.get("/friendslocations", async (req, res) => {
-    const friendIds = req.query.friendIds;
-    let locations = {};
-    let parsedIds = friendIds.split(',');
-    for (let friendId of parsedIds) {
-        let location = await TrackedLocation.findOne({ webId: friendId }).sort({ timestamp: -1 });
-        if (location != null)
-            locations[friendId] = location;
-    }
-
-    res.send(locations);
 });
 
 module.exports = router;
--- restapi/middleware/FriendsStore.js
+++ restapi/middleware/FriendsStore.js
@@ -1,8 +1,36 @@
-const QuickLRU = require("quick-lru");
+const c = require("@thi.ng/cache");
+const rdf = require("rdflib");
+
+const REFRESH_INTERVAL = 300000; //5 min
+const CACHE_LENGTH = 100;
 
 class FriendsStore {
     constructor() {
-        this.cache = new QuickLRU({ maxSize: 1000 });
+        this._cache = new c.TLRUCache(null, { maxlen: CACHE_LENGTH, ttl: REFRESH_INTERVAL });
+    }
+    async areFriends(webId, friendWebId) {
+        if (this._cache.has(webId))
+            return this._cache.get(webId).includes(friendWebId);
+
+        if (this._cache.has(friendWebId))
+            return this._cache.get(friendWebId).includes(webId);
         
+        const friends = await this.fetchFriends(webId);
+        return this._cache.set(webId, friends).includes(friendWebId);
+    }
+    async fetchFriends(webId) {
+        const store = rdf.graph();
+        const FOAF = rdf.Namespace("http://xmlns.com/foaf/0.1/");
+        const fetcher = new rdf.Fetcher(store);
+
+        const me = store.sym(webId);
+        await fetcher.load(webId);
+        const names = store.each(me, FOAF("knows"));
+        const namesDocs = names.map(name => name.doc());
+        await fetcher.load(namesDocs);
+        return store.each(null, FOAF("knows"), me)
+            .map(webId => webId.value);
     }
 }
+const friendsStore = new FriendsStore();
+module.exports = friendsStore;
