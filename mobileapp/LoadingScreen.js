29e95df8831836ff24fcae09bf49fb3f5e8714b2
user.entry <uo270927@uniovi.es>
2021-05-01 10:10:40 +0200

    Fixed a bug
    The bug displayed in the console tons of messages and that was because
    of some unhandled exception

--- mobileapp/LoadingScreen.js
+++ mobileapp/LoadingScreen.js
@@ -72,7 +72,7 @@ export default function LoadingScreen({ route, navigation }) {
                 if (AsyncStorage.getItem("userId") !== null && AsyncStorage.getItem("userId") !== undefined && AsyncStorage.getItem("userId") != "" ){
                   let prevFriends = getFriendsNames();
                 dispatch(fetchFriends());
-                let newFriends = getFriendsNames().filter(friend => !(prevFriends.includes(friend)));
+                let newFriends = getFriendsNames()?.filter(friend => !(prevFriends.includes(friend)));
                 if (newFriends.length > 0)
                 schedulePushNotificationFriends(newFriends);
                 }
