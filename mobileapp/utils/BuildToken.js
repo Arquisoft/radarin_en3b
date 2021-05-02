a3dffe63744c805e9cd3f9a57ff3da54f7a81ac6
alvarorece <alvarorece@gmail.com>
2021-05-02 20:41:13 +0200

    more!

--- mobileapp/utils/BuildToken.js
+++ mobileapp/utils/BuildToken.js
@@ -2,13 +2,13 @@ import RS256JWTBuilder from "./BuildRS256JWT";
 import * as SecureStore from "expo-secure-store";
 
 export default async function () {
-    const p = await SecureStore.getItemAsync("op234iyu5v6oy234iuv6");
-    const parsed = JSON.parse(p);
-    const userId = parsed.webId;
-    const privateKey = parsed.privateKey;
-    const payload = {
-        sub: "test",
-        webid: userId
-    };
-    return RS256JWTBuilder(privateKey, payload);
+  const p = await SecureStore.getItemAsync("op234iyu5v6oy234iuv6");
+  const parsed = JSON.parse(p);
+  const userId = parsed.webId;
+  const privateKey = parsed.privateKey;
+  const payload = {
+    sub: "test",
+    webid: userId
+  };
+  return RS256JWTBuilder(privateKey, payload);
 }
