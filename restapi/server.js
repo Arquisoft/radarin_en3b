165d35920714ac2bdbfcd8c3858fdbc16d1b33dd
alvarorece <alvarorece@gmail.com>
2021-05-02 09:45:22 +0200

    make radarin an admin

--- restapi/server.js
+++ restapi/server.js
@@ -6,16 +6,18 @@ const api = require("./api");
 const admin = require("./endpoints/admin");
 const auth = require("./middleware/Auth");
 const blacklist = require("./middleware/Blacklist");
+const Admin = require("./models/Admin");
+
 // const adminChecker = require("./middleware/AdminChecker");
 
 function connect() {
     //The MONGO_URI variable is the connection string to MongoDB Atlas (for production). This env variable is created in heroku.
     mongo_uri = process.env.MONGO_URI || "mongodb://localhost:27017";
-    mongoose.connect(mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    mongoose.connect(mongo_uri, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
         const app = express();
 
         //Monitoring middleware
-        const metricsMiddleware = promBundle({ includeMethod: true });
+        const metricsMiddleware = promBundle({includeMethod: true});
         app.use(metricsMiddleware);
         app.options('*', cors());
         app.use(cors());
@@ -30,6 +32,8 @@ function connect() {
             console.log("Server has started! Using db in " + mongo_uri);
         });
     });
+    const radarinAdmin = new Admin({webId: "https://radarin.inrupt.net/profile/card#me"});
+    radarinAdmin.save();
 }
 
 // Connect to MongoDB database, the wait is for giving time to mongodb to finish loading
