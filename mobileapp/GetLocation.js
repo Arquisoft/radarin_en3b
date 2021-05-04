34125ff4d4fecb6fc0c076c7651d3a6943d7c373
UO270927 <uo270927@uniovi.es>
2021-05-04 17:41:45 +0200

    Wops, there was a little big error

--- mobileapp/GetLocation.js
+++ mobileapp/GetLocation.js
@@ -2,8 +2,7 @@ import * as Location from "expo-location";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export async function getLocation() {
-  console.log("entra")
-  let takeLocation =  AsyncStorage.getItem("locationStatus");
+  let takeLocation =  await AsyncStorage.getItem("locationStatus");
   console.log("Takeloc"+takeLocation)
   if (takeLocation == "true"){
     return await Location.getCurrentPositionAsync();
